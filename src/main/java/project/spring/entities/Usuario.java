0f33f9695b83b08322d72fa0977af75579210971
MiroTragico12 <jorge.santista10@hotmail.com>
2025-04-16 20:41:17 -0300

    projeto configurado, criando entidades, iniciando normalmente, tudo ok

---
+++ src/main/java/project/spring/entities/Usuario.java
@@ -0,0 +1,67 @@
+package project.spring.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	private String email;
+	private Integer idade;
+	
+	public Usuario(String nome, String email, Integer idade) {
+		this.nome = nome;
+		this.email = email;
+		this.idade = idade;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	
+	
+
+	
+	
+}
